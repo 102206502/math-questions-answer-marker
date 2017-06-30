493f12ae757042049e79a4e110d3f679981ff7cc
102206502 <102206502@users.noreply.github.com>
2017-06-30 17:30:51 +0800

    開始準備取出樣本特徵

--- answer marker/AnswerMarkerer.py
+++ answer marker/AnswerMarkerer.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import re
+from datetime import datetime
 
 class QuestionSolutions(object):
 	"""一個題目，內含一個以上的解題法"""
@@ -13,6 +14,9 @@ class QuestionSolutions(object):
 		----------
 		answer_sheet_file_name : string
 			被評分的文字檔名
+
+		line_finish_time_file_name : string
+			每行完成時間的文字檔名
 
 		pseudocode 
 		-----------
@@ -28,15 +32,16 @@ class QuestionSolutions(object):
 		# end for
 		# return max_score
 	'''
-	def get_score(self, answer_sheet_file_name, output_file):
-		answer_lines = self.read_answer_sheet(answer_sheet_file_name)
+	def get_score(self, answer_sheet_file_name, line_finish_time_file_name, output_file):
+		answer_lines = self.read_file_in_lines(answer_sheet_file_name)
+		time_lines = self.read_file_in_lines(line_finish_time_file_name)
 		max_score = 0.0
 		# calculate score
-		solution_count = 1
-		for solution in self.math_solutions:
+		for solution_count, solution in enumerate(self.math_solutions):
+			solution_count += 1
 			output_file.write('solution ' + str(solution_count) + '\n')
 			print 'solution ' + str(solution_count) + '\n'
-			solution_score = solution.get_score(answer_lines, output_file)
+			solution_score = solution.get_score(answer_lines, time_lines, output_file)
 			output_file.write('solution ' + str(solution_count) + ' 正確率 : ' + str(solution_score) + '\n\n')
 			print 'solution ' + str(solution_count) + ' 正確率 : ' + str(solution_score) + '\n\n'
 			if solution_score > max_score:
@@ -45,24 +50,25 @@ class QuestionSolutions(object):
 			if max_score >= 1:
 				break
 
-			solution_count += 1
 
 		return max_score
 
-	def read_answer_sheet(self, answer_sheet_file_name):
-		answer_lines = []
-		file_answer = open(answer_sheet_file_name, 'rt')
+	def read_file_in_lines(self, file_name):
+		lines = []
+		file = open(file_name, 'rt')
+		str_buffer = ''
 		while True:
-			line = file_answer.readline()
-			if not line:
+			line = file.readline()
+			if not line or line == '\n':
 				break
+			str_buffer+=line
+		file.close()
+		lines = str_buffer.split('\n')
+		if lines[len(lines)-1] == '':
+			del  lines[-1]
+		print 'read file\n', lines
 
-			answer_lines.append(line)
-
-		file_answer.close()
-		print 'read file\n', answer_lines
-
-		return answer_lines
+		return lines
 
 	def addSolution(self, solution):
 		self.math_solutions.append(solution)
@@ -79,12 +85,54 @@ class MathSolution(object):
 		step.number = self.step_count
 		self.steps.append(step)
 
-	def get_score(self, answer, output_file):
+	def get_score(self, answer_lines, time_lines, output_file):
+		FMT = '%H:%M:%S.%f0'
+		step_scores = 0.0
 		step_score = 0.0
-		for step in self.steps:
-			step_score += step.get_score(answer, output_file)
+		temp_step_time = datetime.strptime('00:00:00.0000000', FMT)
+		temp_step_type = '計算'
+		for line_idx, ans_line in enumerate(answer_lines):
+			print 'line', line_idx, ans_line
+			for step_idx, step in enumerate(self.steps):
+				line_score = step.get_score(ans_line, time_lines, output_file)
+				step_scores += line_score
+				if line_score > 0:
+					if temp_step_type == step.step_type:
+						# 和上個時間相加
+						temp_delta = datetime.strptime(time_lines[line_idx], FMT) - datetime.strptime('00:00:00.0000000', FMT)
+						temp_step_time += temp_delta
+						step_score += line_score
+						print 'step_score += to', step_score
+					else:
+						if line_idx > 0:
+							if temp_step_type != '計算':
+								output_file.write(temp_step_type+', '+temp_step_time.strftime(FMT)+', 正確率 '+str(step_score)+'\n')
+							else:
+								output_file.write(temp_step_type+', '+temp_step_time.strftime(FMT)+'\n')
+						step_score = line_score
+						print 'step_score =', step_score
+						temp_step_time = datetime.strptime(time_lines[line_idx], FMT)
+						temp_step_type = step.step_type
+					break
+				elif step_idx == len(self.steps)-1:# 若本行文件完全不屬任一步驟
+					if temp_step_type == '計算':
+						# 和上個時間相加
+						temp_delta = datetime.strptime(time_lines[line_idx], FMT) - datetime.strptime('00:00:00.0000000', FMT)
+						temp_step_time += temp_delta
+					else:
+						if line_idx > 0:
+							output_file.write(temp_step_type+', '+temp_step_time.strftime(FMT)+', 正確率 '+str(step_score)+'\n')
+						temp_step_type = '計算'
+						temp_step_time = datetime.strptime(time_lines[line_idx], FMT)
+			# output_file.write(temp_step_type+', line '+str(line_idx)+', '+time_lines[line_idx]+'\n')
+			
+			if line_idx == len(answer_lines)-1:
+				if temp_step_type != '計算':
+					output_file.write(temp_step_type+', '+temp_step_time.strftime(FMT)+', 正確率 '+str(step_score)+'\n')
+				else:
+					output_file.write(temp_step_type+', '+temp_step_time.strftime(FMT)+'\n')
 
-		score = step_score/len(self.steps)
+		score = step_scores/len(self.steps)
 		return score
 
 class StepOfSolution(object):
@@ -106,6 +154,7 @@ class StepOfSolution(object):
 		#self.keys = add_by_tmp_keys(tmp_keys)
 		self.keys = []
 		self.number = 0 # 0相當於未設定順序編號
+		self.step_type = ''
 		self.step_type = step_type
 
 	'''加入關鍵正規式
@@ -125,37 +174,32 @@ class StepOfSolution(object):
 	'''計算該步驟成績
 		Parameter
 		----------
-		answer : 1D string list
-			lines of the answer sheet
+		answer : string
+			a line of the answer sheet
 		output_file : file obj
 			marked result file
 	'''
-	def get_score(self, answer, output_file):
+	def get_score(self, answer, time_lines, output_file):
 		match_count = 0.0
 
 		for key in self.keys:
-			print 'key:', key
-			for line_count, ans_line in enumerate(answer):
-				matches = re.finditer(key, ans_line)
+			matches = re.finditer(key, answer)
+			# print key, answer
+			result = False
 
-				result = False
+			for matchNum, match in enumerate(matches):
+				matchNum = matchNum + 1
+				if matchNum:
+					result = True
 
-				for matchNum, match in enumerate(matches):
-					matchNum = matchNum + 1
-					if matchNum:
-						result = True
-
-				if result:
-					match_count += 1
-					#self.step_type
-					break
+			if result:
+				match_count += 1
 
 		score = 0.0
 		score = match_count/len(self.keys)
-		output_file.write('keys len:' + str(len(self.keys)) + '\n')
-		print 'keys len:', len(self.keys)
-		output_file.write('step ' + self.content + '\nmatch count : ' + str(match_count) + '\n正確率 ' + str(score) + '\n')
-		print 'step ' + self.content + '\nmatch count : ' + str(match_count) + '\n正確率 ' + str(score)
+		# if score > 0:
+		# 	print self.step_type, '\nstep ' + self.content + '\n正確率 ' + str(score)
+		# print self.step_type, '\nstep ' + self.content + '\n正確率 ' + str(score)
 		return score
 
 
---
+++ answer marker/feature_to_vecter.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+import AnswerMarkerer
+import set_bucket_solution
+import set_payForPen_solution
+
+question2_file_nums = [39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 61, 62, 63, 64, 65, 66]
