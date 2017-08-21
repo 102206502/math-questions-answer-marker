a13937d6bd1d502cbdbae2a7cdf2f5c3ec046322
102206502 <102206502@cc.ncu.edu.tw>
2017-08-21 16:33:36 +0800

    註解畫表部分

--- answer marker/train.py
+++ answer marker/train.py
@@ -25,20 +25,20 @@ predicted = clf.predict(X_test)
 print(metrics.classification_report(Y_test, predicted))
 print(metrics.confusion_matrix(Y_test, predicted))
 
-X_iso = Isomap(n_neighbors=10).fit_transform(X_train)
+# X_iso = Isomap(n_neighbors=10).fit_transform(X_train)
 
-fig, ax = plt.subplots(1, 2, figsize=(8, 4))
+# fig, ax = plt.subplots(1, 2, figsize=(8, 4))
 
-fig.subplots_adjust(top=0.85)
+# fig.subplots_adjust(top=0.85)
 
-ax[0].scatter(X_iso[:, 0], X_iso[:, 1], c=predicted)
-ax[0].set_title('Predicted labels')
-ax[1].scatter(X_iso[:, 0], X_iso[:, 1], c=y_train)
-ax[1].set_title('Actual Labels')
+# ax[0].scatter(X_iso[:, 0], X_iso[:, 1], c=predicted)
+# ax[0].set_title('Predicted labels')
+# ax[1].scatter(X_iso[:, 0], X_iso[:, 1], c=y_train)
+# ax[1].set_title('Actual Labels')
 
-# 加入標題
-fig.suptitle('Predicted versus actual labels', fontsize=14, fontweight='bold')
+# # 加入標題
+# fig.suptitle('Predicted versus actual labels', fontsize=14, fontweight='bold')
 
-# 顯示圖形
-plt.show()
+# # 顯示圖形
+# plt.show()
 
