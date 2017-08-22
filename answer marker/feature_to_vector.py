624030b57123582a057bd0e708fe6b47cff53f6f
102206502 <102206502@cc.ncu.edu.tw>
2017-08-22 16:26:36 +0800

    deal with some merge conflict

--- answer marker/feature_to_vector.py
+++ answer marker/feature_to_vector.py
@@ -7,16 +7,11 @@ import time_plus
 import numpy as np
 
 
-<<<<<<< HEAD
-file_nums_question1 = [37, 38, 52, 53, 55, 56, 58]
-file_nums_question2 = [39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 51, 61, 62, 63, 64, 65, 66]
-=======
 student_state_dic = {'well' : 0, '不會列方程式' : 1, '計算錯誤' : 2, '計算太慢' : 3, '公式不熟' : 4, '完全不會' : 5}
 file_nums_question1 = [37, 38, 52, 53, 55, 56, 58]
 file_nums_question2 = [39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 50, 51, 61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73]
 file_nums_question_list = [file_nums_question1, file_nums_question2]
 student_state_list  = [ 0,  0,  0,  1,  2,  2,  3,  1,  1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  3,  4,  4,  4,  4,  4,  4]
->>>>>>> python3
 FMT = '%H:%M:%S.%f0'
 '''取得同類題目的全部資料'''
 def get_answers_result(question_file_nums, question_mode):
@@ -28,11 +23,7 @@ def get_answers_result(question_file_nums, question_mode):
 		marked_result = math_question.get_marked_data()
 		marked_result.append(math_question.total_cost_time())
 		marked_results_list.append(marked_result)
-<<<<<<< HEAD
 		# get_score.write_marked_result(math_question, str(file_num))
-=======
-		get_score.write_marked_result(math_question, str(file_num))
->>>>>>> python3
 
 	return marked_results_list
 
@@ -44,10 +35,7 @@ def get_average_score(question_file_nums, marked_results_list):
 		# print question_file_nums[idx_file_num], cur_score
 		total += cur_score
 	avg_score = total/len(question_file_nums)
-<<<<<<< HEAD
-=======
 	print('avg_score', avg_score)
->>>>>>> python3
 	return avg_score
 
 '''得出所有樣本平均花費時間'''
@@ -55,16 +43,10 @@ def get_average_cost_time(question_file_nums, marked_results_list):
 	total = '00:00:00.0000000'
 	for idx_file_num, marked_result in enumerate(marked_results_list):
 		cur_time = marked_result[len(marked_result)-1]
-<<<<<<< HEAD
-		# print question_file_nums[idx_file_num], cur_score
-		total = time_plus.time_str_plus(FMT, total, cur_time)
-	avg_time = time_plus.time_str_divide(FMT, total, len(question_file_nums))
-=======
 		# print question_file_nums[idx_file_num], cur_time
 		total = time_plus.time_str_plus(FMT, total, cur_time)
 	avg_time = time_plus.time_str_divide(FMT, total, len(question_file_nums))
 	print('avg_time', avg_time)
->>>>>>> python3
 	return avg_time
 
 '''得出每個步驟的平均'''
@@ -77,11 +59,7 @@ def get_average_steps_score(question_file_nums, marked_results_list):
 			cur_score = marked_result[i][1]
 			total += cur_score
 		steps_avg_score.append(total/(len(marked_results_list)))
-<<<<<<< HEAD
-	
-=======
 	print('steps_avg_score', steps_avg_score)
->>>>>>> python3
 	return steps_avg_score
 
 '''得出每個步驟的平均時間，去掉時間是0的樣本'''
@@ -101,61 +79,20 @@ def get_average_steps_time(question_file_nums, marked_results_list):
 				non_zero_sample += 1
 		temp_step_avg_time_str = time_plus.time_str_divide(FMT, total, non_zero_sample)
 		steps_avg_time.append(time_plus.time_str_to_float(FMT, temp_step_avg_time_str))
-<<<<<<< HEAD
-=======
 	print('steps_avg_time', steps_avg_time)
->>>>>>> python3
 	return steps_avg_time
 
 '''製作解答特徵的陣列'''
 def make_feature_vector_all(question_file_nums, marked_results_list):
 	# 取出多份檔案的平均值
-<<<<<<< HEAD
-=======
 	all_feature_vector_list = []
 	avg_score = get_average_score(question_file_nums, marked_results_list)
->>>>>>> python3
 	avg_time = get_average_cost_time(question_file_nums, marked_results_list)
 	steps_avg_score = get_average_steps_score(question_file_nums, marked_results_list)
 	steps_avg_time = get_average_steps_time(question_file_nums, marked_results_list)
 
 	# 取出單份答案的資料
 	for idx_file_num, marked_result in enumerate(marked_results_list):
-<<<<<<< HEAD
-		step_status_list = make_feature_vector(idx_file_num, marked_result)
-		# the correctness of this answer(by the last step)
-		if marked_result[len(marked_result)-2] == 0 :
-			step_is_correct_list.append(-1)
-		else:
-			step_is_correct_list.append(1)
-
-def make_feature_vector(idx_file_num, marked_result):# 未完成
-	step_status_list = []# 分數 對錯 時間
-		step_score_list = []
-		step_is_correct_list = []
-		step_times = []
-		step_len = len(marked_result)-2
-		for i in range(step_len):
-			step_score_list.append(marked_result[i][1])# step score of this answer
-			if marked_result[step_len-1][1] >= 1:
-				step_is_correct_list.append(1)# 該步驟錯誤應是因為手寫辨識問題
-			elif marked_result[i][1] >= 1:
-				step_is_correct_list.append(1)# 該步驟正確
-			elif marked_result[i][1] > 0:
-				step_is_correct_list.append(0)# 該步驟不完全正確
-			else:
-				step_is_correct_list.append(-1)# 該步驟完全錯誤
-			step_times.append(marked_result[i][2])# step time of this answer
-	return step_status_list
-
-############################################################################################
-
-alist = get_answers_result(file_nums_question2, 2)
-avg_score = get_average_score(file_nums_question2, alist)
-make_feature_vector_all(file_nums_question2, alist)
-# for data in alist:
-# 	print data
-=======
 		step_status_list = make_step_feature_vector(idx_file_num, marked_result)
 		feature_vector = step_status_list
 		# print(feature_vector)
@@ -214,4 +151,3 @@ def get_all_data(question_mode):
 marked_results_list = get_answers_result(file_nums_question2, 2)
 X_data = make_feature_vector_all(file_nums_question2, marked_results_list)
 Y_data = np.array(student_state_list)
->>>>>>> python3
